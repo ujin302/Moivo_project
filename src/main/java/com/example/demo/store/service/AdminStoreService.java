117f44280fff001c5e836c39669828b1c198f6e0
Your Name <your.email@example.com>
2024-12-16 09:19:21 +0900

    머징및 AdminStoreService에 @Servcie 생성

--- src/main/java/com/example/demo/store/service/AdminStoreService.java
+++ src/main/java/com/example/demo/store/service/AdminStoreService.java
@@ -2,6 +2,9 @@ package com.example.demo.store.service;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface AdminStoreService {
 
     Map<String, Object> getProductStatus();
