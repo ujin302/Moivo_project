597e426705a5a671c00c0d57aea57d942a9bbc32
유진 <82002058+ujin302@users.noreply.github.com>
2024-12-20 16:25:02 +0900

    Update vite.config.js

--- src/main/moivo_react/vite.config.js
+++ src/main/moivo_react/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 상대 경로로 설정
+  build: {
+      outDir: '../resources/build',
+  },
   plugins: [react()],
   server: {
     proxy: {
