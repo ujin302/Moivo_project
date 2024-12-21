776daa7cded5b88bef139a7530f782798143fa67
유진 <82002058+ujin302@users.noreply.github.com>
2024-12-21 18:34:58 +0900

    Update path.js

--- src/main/moivo_react/scripts/path.js
+++ src/main/moivo_react/scripts/path.js
@@ -1,6 +1,6 @@
 export const PATH = {
-    // SERVER: 'http://101.79.9.149:9090', // 서버 주소
-    SERVER: 'http://localhost:9090', // 서버 주소
+    SERVER: 'http://101.79.9.149:9090', // 서버 주소
+    // SERVER: 'http://localhost:9090', // 서버 주소
     // SERVER: 'http://127.0.0.1:9090', // 서버 주소
     CLIENT: 'http://localhost:5173' // 클라이언트 주소
 };
